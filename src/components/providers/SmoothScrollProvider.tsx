5688db2ccb1d21d1e3f8eb2c5f0b7df9e7b89a8e
DR-JOSHUA009 <fizcleascnol@gmail.com>
2026-03-24 04:01:36 -0600

    intento 3 de arreglo

--- src/components/providers/SmoothScrollProvider.tsx
+++ src/components/providers/SmoothScrollProvider.tsx
@@ -9,6 +9,7 @@ export default function SmoothScrollProvider({
 }) {
   return (
     <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+      {/* @ts-expect-error: React 19 ReactNode type mismatch with Lenis older types */}
       {children}
     </ReactLenis>
   );
